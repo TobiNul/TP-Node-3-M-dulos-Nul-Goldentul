36a4107bb4e78281100f86e13a9f966706492dcb
Tomas Goldentul <goldentultomas@gmail.com>
2026-04-10 08:06:22 -0300

    Update product search query to 'Mouse'

--- src/ej05.js
+++ src/ej05.js
@@ -1,5 +1,5 @@
 import { buscarProducto } from "./modules/buscarProducto.js";
-const producto = buscarProducto("edede");
+const producto = buscarProducto("Mouse");
 if (producto !== null) {
     console.log("Producto encontrado");
     console.log(`Producto: ${producto.nombre}`);
